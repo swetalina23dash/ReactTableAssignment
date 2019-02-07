364d74d6011af1a295bb1aaf070afbd76d8b1556
swetalina <swetalina23.dash@gmail.com>
2019-02-06 22:55:32 -0800

    no message

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express()
 const employees = require('./data/employees.json');
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+let db = new sqlite3.Database('employee.db');
 
 var corsOptions = {
   origin: 'http://localhost:3000',
@@ -18,48 +18,99 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/api/employees', cors(corsOptions), (req, res, next) => {
-  console.log('/api/employees');
   res.setHeader('Content-Type', 'application/json');
-  
- 
-  db.serialize(function() {
-    db.run("CREATE TABLE if not exist mytable (info TEXT)");
-   
-    var stmt = db.prepare("INSERT INTO mytable VALUES (?)");
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
+  let sql="Select * from Employee";
+  let employeeJson=[];
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
     }
-    stmt.finalize();
-   
-    db.each("SELECT rowid AS id, info FROM mytable", function(err, row) {
-        console.log(row.id + ": " + row.info);
+    rows.forEach((row) => {
+      employeeJson.push({"id":row.id,
+                        "name": row.name,
+                        "code": row.code,
+                        "profession": row.profession,
+                        "color": row.color,
+                        "city": row.city,
+                        "branch": row.branch,
+                        "assigned": row.assigned
+    });
+      
     });
   });
-   
+ 
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+  });
+    res.status(200);
+    res.send(JSON.stringify(employeeJson, null, 2));
+})
+
+app.post('/api/addEmployees', cors(corsOptions), (req, res, next) => {
+   res.setHeader('Content-Type', 'application/json');
+   db.serialize(() => { 
+   db.run("CREATE TABLE if not exists Employee (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,code TEXT,profession TEXT,color TEXT,city TEXT,branch TEXT,assigned TEXT)")
+     .run('INSERT INTO Employee (name, code, profession, color,city,branch,assigned) VALUES (?, ?, ?, ?,?,?,?)', [req.name,req.code,req.profession,req.color,req.city,req.branch,req.assigned], function(err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+    });
+  });
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+  });
+  res.status(200);
+  res.send(JSON.stringify(employees, null, 2));
+})
+
+app.delete('/api/deleteEmployees', cors(corsOptions), (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
   
+  let id = 1;
+  // delete a row based on id
+  db.run('DELETE FROM Employee WHERE rowid=?', id, function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Row(s) deleted ${this.changes}');
+  });
+
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+  });
   res.status(200);
   res.send(JSON.stringify(employees, null, 2));
 })
 
-app.get('/api/addEmployees', cors(corsOptions), (req, res, next) => {
-  console.log('/api/addEmployees');
+app.put('/api/updateEmployees', cors(corsOptions), (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
+  let data = ['Kyle Lowry1', 'Kyle Lowry'];
+  let sql = `UPDATE Employee
+              SET name = ?
+              WHERE name = ?`;
+   
+  db.run(sql, data, function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(`Row(s) updated: ${this.changes}`);
+   
+  });
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+  });
   res.status(200);
   res.send(JSON.stringify(employees, null, 2));
 })
 
-app.get('/api/deleteEmployees', cors(corsOptions), (req, res, next) => {
-  console.log('/api/deleteEmployees');
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200);
-  res.send(JSON.stringify(employees, null, 2));
-})
-
-app.get('/api/updateEmployees', cors(corsOptions), (req, res, next) => {
-  console.log('/api/updateEmployees');
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200);
-  res.send(JSON.stringify(employees, null, 2));
-})
 
 app.listen(8080, () => console.log('Job Dispatch API running on port 8080!'))
