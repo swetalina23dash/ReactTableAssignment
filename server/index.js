acf3277a5eb804f1ece8987aa6547bfac1402492
swetalina <swetalina23.dash@gmail.com>
2019-02-19 21:17:34 -0800

    no message

--- server/index.js
+++ server/index.js
@@ -15,101 +15,94 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
  
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.get('/api/employees', cors(corsOptions), (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
   let sql="Select * from Employee";
-  let employeeJson=[];
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    rows.forEach((row) => {
-      employeeJson.push({"id":row.id,
-                        "name": row.name,
-                        "code": row.code,
-                        "profession": row.profession,
-                        "color": row.color,
-                        "city": row.city,
-                        "branch": row.branch,
-                        "assigned": row.assigned
-    });
-      
-    });
-  });
+  let params=[];
  
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
+db.serialize(() => {
+db.all(sql, params, (err, rows) => {
+  if (err) {
+    res.status(400).json({"error":err.message});
+    return;
+  }
+  res.status(200);
+  res.json({
+      "message":"success",
+      "data":rows
   });
-    res.status(200);
-    res.send(JSON.stringify(employeeJson, null, 2));
+});
+}); 
 })
 
-app.post('/api/addEmployees', cors(corsOptions), (req, res, next) => {
+app.post('/api/addEmployees',  (req, res, next) => {
    res.setHeader('Content-Type', 'application/json');
+  
    db.serialize(() => { 
    db.run("CREATE TABLE if not exists Employee (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,code TEXT,profession TEXT,color TEXT,city TEXT,branch TEXT,assigned TEXT)")
-     .run('INSERT INTO Employee (name, code, profession, color,city,branch,assigned) VALUES (?, ?, ?, ?,?,?,?)', [req.name,req.code,req.profession,req.color,req.city,req.branch,req.assigned], function(err) {
+     .run('INSERT INTO Employee (name, code, profession, color,city,branch,assigned) VALUES (?, ?, ?, ?,?,?,?)', [req.body.name,req.body.code,req.body.profession,req.body.color,req.body.city,req.body.branch,req.body.assigned], function(err) {
       if (err) {
-        return console.log(err.message);
+        res.status(400).json({"error":err.message});
+        return;
       }
       console.log(`A row has been inserted with rowid ${this.lastID}`);
+      res.status(200);
+      res.json({
+          "message":"success",
+          "id":`${this.lastID}`
+      });
     });
   });
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-  res.status(200);
-  res.send(JSON.stringify(employees, null, 2));
+  
+  
 })
 
-app.delete('/api/deleteEmployees', cors(corsOptions), (req, res, next) => {
+app.get('/api/deleteEmployees/:id', cors(corsOptions), (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
   
-  let id = 1;
+  let id = req.params.id;
   // delete a row based on id
+  db.serialize(() => { 
   db.run('DELETE FROM Employee WHERE rowid=?', id, function(err) {
     if (err) {
-      return console.error(err.message);
+      res.status(400).json({"error":err.message});
+      return;
     }
-    console.log('Row(s) deleted ${this.changes}');
+    res.status(200);
+    res.json({
+        "message":"success"
+    });
+    console.log(`Row(s) deleted ${this.changes}`);
   });
-
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-  res.status(200);
-  res.send(JSON.stringify(employees, null, 2));
+});
 })
 
-app.put('/api/updateEmployees', cors(corsOptions), (req, res, next) => {
+app.post('/api/updateEmployees', (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
-  let data = ['Kyle Lowry1', 'Kyle Lowry'];
+  let data = [req.body.name, req.body.code, req.body.profession, req.body.color,req.body.city,req.body.branch,req.body.assigned,req.body.id];
   let sql = `UPDATE Employee
-              SET name = ?
-              WHERE name = ?`;
-   
+              SET name = ?,code= ?, profession= ?, color= ?,city= ?,branch= ?,assigned= ?
+              WHERE id = ?`;
+  db.serialize(() => {  
   db.run(sql, data, function(err) {
     if (err) {
-      return console.error(err.message);
+      res.status(400).json({"error":err.message});
+      return;
     }
     console.log(`Row(s) updated: ${this.changes}`);
-   
+    res.status(200);
+    res.json({
+        "message":"success"
+    });
   });
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-  res.status(200);
-  res.send(JSON.stringify(employees, null, 2));
+});
 })
 
 
--- src/App.js
+++ src/App.js
@@ -1,43 +1,265 @@
 import React from 'react';
+import ReactTable from "react-table";
+import 'react-table/react-table.css';
+import Modal from 'react-responsive-modal';
+import axios from "axios";
+
 
 class App extends React.Component {
-  state = {
-    employees: []
+  constructor(props) {
+  super(props);
+  this.state = {
+    
+    employees: [],
+    message:'',
+    open:false,
+    name: '',
+    code: '',
+    profession: '',
+    color: '',
+    city: '',
+    branch: '',
+    assigned: ''
   }
-  
+  this.upDateEmployee = this.upDateEmployee.bind(this);
+  this.deleteEmployee = this.deleteEmployee.bind(this);
+  this.addEmployee = this.addEmployee.bind(this);
+  this.renderEditable = this.renderEditable.bind(this);
+  this.handleChange = this.handleChange.bind(this);
+  this.handleChangeCode = this.handleChangeCode.bind(this);
+  this.handleChangeColor = this.handleChangeColor.bind(this);
+  this.handleChangeProf = this.handleChangeProf.bind(this);
+  this.handleChangeCity = this.handleChangeCity.bind(this);
+  this.handleChangeBranch = this.handleChangeBranch.bind(this);
+  this.handleChangeAssigned = this.handleChangeAssigned.bind(this);
+}
+
+handleChange(event) {
+  this.setState({name: event.target.value});
+}
+
+handleChangeCode(event) {
+  this.setState({code: event.target.value});
+}
+
+handleChangeColor(event) {
+  this.setState({color: event.target.value});
+}
+
+handleChangeProf(event) {
+  this.setState({profession: event.target.value});
+}
+
+handleChangeCity(event) {
+  this.setState({city: event.target.value});
+}
+
+handleChangeBranch(event) {
+  this.setState({branch: event.target.value});
+}
+
+handleChangeAssigned(event) {
+  this.setState({assigned: event.target.value});
+}
+
+onOpenModal = () => {
+  this.setState({ open: true });
+};
+
+onCloseModal = () => {
+  this.setState({ open: false });
+};
+
+handleInputChange = (cellInfo, event) => {
+    let data = [...this.state.employees];
+  data[cellInfo.index][cellInfo.column.id] = event.target.value;
+
+  this.setState({ employees:data });
+};
+
+renderEditable = cellInfo => {
+  const cellValue = this.state.employees[cellInfo.index][cellInfo.column.id];
+
+  return (
+    <input
+      placeholder="type here"
+      name="input"
+      type="text"
+      onChange={this.handleInputChange.bind(null, cellInfo)}
+      value={cellValue}
+    />
+  );
+};
+
   componentWillMount = () => {
-    fetch('http://localhost:8080/api/employees')
-      .then(response => response.json())
-      .then(employees => this.setState({ employees }))
+      axios.get('http://localhost:8080/api/employees')
+      .then(response => {
+        this.setState({ employees : response.data.data});
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }
+
+  upDateEmployee(rowArr){
+    const obj = {
+       "id":rowArr.original.id,
+       "name": rowArr.original.name,
+       "code": rowArr.original.code,
+       "profession": rowArr.original.profession,
+       "color": rowArr.original.color,
+       "city": rowArr.original.city,
+       "branch": rowArr.original.branch,
+       "assigned": rowArr.original.assigned
+     };
+    axios.post('http://localhost:8080/api/updateEmployees', obj)
+        .then(res => console.log(res.data.message))
+        .catch(err => console.log(err))
+  }
+
+  deleteEmployee(rowArr){
+    
+    axios.get('http://localhost:8080/api/deleteEmployees/'+rowArr.original.id)
+    .then(res => {
+      let employees = this.state.employees;
+      employees.forEach(function(result, index) {
+        if(result['id'] === rowArr.original.id) {
+          employees.splice(index, 1);
+        }    
+      });
+    this.setState({employees: employees});
+    }).catch(err => console.log(err))
+  }
+
+  addEmployee(arr){
+    
+    var employees = this.state.employees
+    
+    const obj = {   
+      "name": this.state.name,
+      "code": this.state.code,
+      "profession": this.state.profession,
+      "color": this.state.color,
+      "city": this.state.city,
+      "branch": this.state.branch,
+      "assigned": this.state.assigned
+    };
+    axios.post('http://localhost:8080/api/addEmployees', obj)
+        .then(res => {
+          
+          employees.push({
+          "id":res.data.id,
+          "name": this.state.name,
+          "code": this.state.code,
+          "profession": this.state.profession,
+          "color": this.state.color,
+          "city": this.state.city,
+          "branch": this.state.branch,
+          "assigned": this.state.assigned
+        });
+        this.setState({employees: employees});
+      }).catch(err => console.log(err))
+  }
+
+  
 
   render() {
     const {
       employees
     } = this.state;
-
-    console.log(this.state);
+   
+    const { open } = this.state;
+   
+   
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name',
+      Cell: this.renderEditable 
+    }, {
+      Header: 'Code',
+      accessor: 'code',
+      Cell: this.renderEditable
+    },{
+      Header: 'Profession',
+      accessor: 'profession',
+      Cell: this.renderEditable
+    }, {
+      Header: 'Color',
+      accessor: 'color',
+      Cell: this.renderEditable
+    },{
+      Header: 'City',
+      accessor: 'city',
+      Cell: this.renderEditable
+    },{
+      Header: 'Branch',
+      accessor: 'branch',
+      Cell: this.renderEditable
+    },{
+      Header: 'Assigned',
+      accessor: 'assigned',
+      Cell: this.renderEditable
+    },{
+      Header: 'Action',
+      id: "id",
+      Cell: row => (
+        <div>
+            <button onClick={() => this.upDateEmployee(row)}>Edit</button>
+            <button onClick={() => this.deleteEmployee(row)}>Delete</button>
+        </div>
+    )
+    }]
+   
 
     return (
       <div className="App">
+        {this.state.message}
         <h1>Plexxis Employees</h1>
-        {
-          employees.map(employee => (
-            <div key={employee.id}>
-              {
-                Object.keys(employee).map(key => 
-                  <span key={key}>
-                    { key }:
-                    { employee[key] } 
-                  </span>
-                )
-              }
-            </div>
-          ))
-        }
+        <ReactTable
+            data={employees}
+            columns={columns} defaultPageSize={5} className="-striped -highlight"
+          /> 
+          <button id="addBtn" onClick={this.onOpenModal}>Add New Row</button>
+         <Modal open={open} onClose={this.onCloseModal} center>
+          <h3>Add New Employee</h3>
+          <form onSubmit={this.addEmployee}>
+          <label>
+            Name:
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value={this.state.name} onChange={this.handleChange} />
+          </label><br/><br/>
+          <label>
+          Code:
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" value={this.state.code} onChange={this.handleChangeCode} />
+          </label><br/><br/>
+          <label>
+          Profession:
+          &nbsp;&nbsp;&nbsp; <input type="text" value={this.state.profession} onChange={this.handleChangeProf} />
+          </label><br/><br/>
+          <label>
+          Color:
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" value={this.state.color} onChange={this.handleChangeColor} />
+          </label><br/><br/>
+          <label>
+          City:
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" value={this.state.city} onChange={this.handleChangeCity} />
+          </label><br/><br/>
+          <label>
+          Branch:
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" value={this.state.branch} onChange={this.handleChangeBranch} />
+          </label><br/><br/>
+          <label>
+          Assigned:
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" value={this.state.assigned} onChange={this.handleChangeAssigned} />
+          </label><br/><br/><br/>
+          <input type="submit" value="Submit" />
+        </form>
+         
+        </Modal>
       </div>
     );
+      
   }
+  
 }
 
 export default App;
